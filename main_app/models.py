3a5a408532fbe90f2c7df70a5fa9da7e9f5cdcd0
Mike Mattingly <cmattingly1584@gmail.com>
2021-07-07 16:46:16 -0700

    adds class based views and one to many model lesson work

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+WORKS = (
+    ('O', 'Oil Changed'),
+    ('T', 'Tune Up'),
+    ('R', 'Tire Rotation'),
+)
+
+
 class Car(models.Model):
     year = models.CharField(max_length=4)
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     comment = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.model
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={'car_id': self.id})
+
+class Maintenance(models.Model):
+    date = models.DateField('maintenance date')
+    work_done = models.CharField(
+        max_length=1,
+        choices=WORKS,
+        default=[0][0]
+    )
+
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_work_done_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Car
+from .forms import MaintenanceForm
 
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# CBV(Class Based View) functions
+
+class CarCreate(CreateView):
+    model = Car
+    fields = '__all__'
+
+class CarUpdate(UpdateView):
+    model = Car
+    fields = '__all__'
+
+class CarDelete(DeleteView):
+    model = Car
+    success_url = '/cars/'
 
 # Create your views here.
+def add_maintenance(request, car_id):
+    form = MaintenanceForm(request.POST)
+
+    if form.is_valid():
+        new_maintenance = form.save(commit=False)
+        new_maintenance.car_id = car_id
+        new_maintenance.save()
+    return redirect('detail', car_id=car_id)
+
 def home(request):
-    return HttpResponse('<h1>Welcome to Car Collector</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -16,4 +40,8 @@ def cars_index(request):
 
 def cars_detail(request, car_id):
     car = Car.objects.get(id=car_id)
-    return render(request, 'cars/detail.html', { 'car': car })
+    maintenance_form = MaintenanceForm()
+    return render(request, 'cars/detail.html', { 
+        'car': car,
+        'maintenance_form': maintenance_form
+        })
